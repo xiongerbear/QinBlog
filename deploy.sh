b78e6fc74e7f46e4f9eac85b9594eba41d8d3763
xiongyoulong <xiongyoulong@tangbull.com>
2020-09-22 18:04:20 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Created by PhpStorm.
+# User: xiongyoulong
+# Date: 2020/9/22
+# Time: 6:00 PM
+
+cd /var/www/QinBlog/
+git checkout master
+git pull
+cd /var/www/QinBlog/
+php artisan config:cache
+php artisan route:cache
