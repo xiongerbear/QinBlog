2e2cfb581cca1b9a3ed941f00b23ef1f3fa318b0
xiongyoulong <xiongyoulong@tangbull.com>
2020-09-23 11:54:22 +0800

    test auto deploy.php

--- public/deploy.php
+++ public/deploy.php
@@ -5,4 +5,14 @@
  * Date: 2020/9/23
  * Time: 11:08 AM
  */
-echo "deploy.php exec";
+
+$deployLogFile = '../storage/logs/deploy.log';
+$date = date('Y-m-d H:i:s');
+$content = "[$date] deploy start \n";
+file_put_contents($deployLogFile, $content, FILE_APPEND);
+$command = "sh ../deploy.sh";
+exec($command);
+$date = date('Y-m-d H:i:s');
+$content = "[$date] deploy complete \n";
+file_put_contents($deployLogFile, $content, FILE_APPEND);
+exit();
